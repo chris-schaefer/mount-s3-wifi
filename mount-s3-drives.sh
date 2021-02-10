ce0130eb997deecaf79cbda00c72184c171f138b
Christian Schäfer <christian.schaefer@deptagency.com>
2021-02-10 19:01:53 +0100

    added allow_other option

--- mount-s3-drives.sh
+++ mount-s3-drives.sh
@@ -5,5 +5,5 @@ sed 1d $HOME/.aws/buckets.csv | while read line; do
   echo $line;
   MountDir=$HOME/S3/$Bucket/$Path;
   [ ! -d "$MountDir" ] && mkdir -p $MountDir;
-  s3fs $Bucket:/$Path $MountDir -o passwd_file=$HOME/.aws/passwd-s3fs -o endpoint=$Region -o url="https://s3-$Region.amazonaws.com"
+  s3fs $Bucket:/$Path $MountDir -o passwd_file=$HOME/.aws/passwd-s3fs -o endpoint=$Region -o url="https://s3-$Region.amazonaws.com" -o allow_other
 done
